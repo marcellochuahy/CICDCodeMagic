31ca1e56046c2d9205f6480b483f557e781b05b4
marcellochuahy <marcellochuahy@icloud.com>
2024-04-23 17:56:24 -0300

    First commit

---
+++ CICDCodeMagic/CICDCodeMagic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDCodeMagic
+//
+//  Created by Marcello Chuahy on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# CICDCodeMagic
+
+WIP
